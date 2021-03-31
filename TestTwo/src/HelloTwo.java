41f840b427f4c184367fa63f5bc5a791d508347f
KK <k349211108@163.com>
2021-03-31 19:59:45 +0800

    testTwo

---
+++ TestTwo/src/HelloTwo.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: Koreleone
+ * @Date: 2021-03-31 19:59
+ * @Description:
+ */
+public class HelloTwo {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
